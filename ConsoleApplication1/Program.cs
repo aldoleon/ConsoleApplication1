774ae69997375dd2695f81aee7dfc24cf178b1fa
aldoleon <aldoleon@aim.com>
2016-07-24 19:15:52 -0700

    for aldo

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello !");  //Firstly written 
+            Console.WriteLine("Hello  aldo !");  //Firstly written 
             Console.ReadLine();
         }
     }
